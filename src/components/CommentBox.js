cac9bb780f72ea253625da4b73980aeed345412c
Logan Hart <2loganhart@gmail.com>
2018-07-26 12:45:11 -0700

    Create actions dir, add SAVE_COMMENT actions, hook action up to comment box component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -9,8 +11,7 @@ class CommentBox extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    // Call an action creator
-    // And save the comment
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
@@ -30,4 +31,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
